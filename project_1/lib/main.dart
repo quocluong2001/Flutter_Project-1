54397ec53f2ba46eced9b83f438faa012b01a16a
Hà Quốc Lương <quocluong912@gmail.com>
2022-04-11 03:20:51 +0700

    Module 2 Lesson 31

--- project_1/lib/main.dart
+++ project_1/lib/main.dart
@@ -5,7 +5,35 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void answerButtonHandler() {
+    print("You've chosen this answer!");
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text("Hello"));
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My First App"),
+        ),
+        body: Column(
+          children: [
+            Text("Question"),
+            ElevatedButton(
+              onPressed: answerButtonHandler,
+              child: Text("Answer 1"),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text("Answer 2"),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text("Answer 3"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
